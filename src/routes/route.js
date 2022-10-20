dc354d35152a6f6580db89631eedb3f6b444880c
shivamp2001 <shivampenshnwar2001@gmail.com>
2022-10-20 18:09:06 +0530

    add arry object in existing arry give condition key name in uniq

--- src/routes/route.js
+++ src/routes/route.js
@@ -32,16 +32,57 @@ let players =
        },
    ]
  
-   router.post('/players', function (req, res) {
-    let name=req.body.name //receive name obj (key and value)send from user
-    let dob=req.body.dob   //receive dob 
-    let gender=req.body.gender //gender
-    let city=req.body.city  //city
-    let sports=req.body.sports //sports
-    players.push({name,dob,gender,city,sports}) //using NJSON obj method create key value pair
-       console.log({ data: players , status: true } )
-       res.send(  { data: players , status: true }  )//send response to user
-   })
+router.post('/players',function(req,res){
+    let body=req.body
+    let addplayer=players.find(x=>x.name===body.name)
+    if(addplayer){
+        return res.send({msg:"name is existing"})
+    }else{
+        players.push(body)
+        return res.send({players})
+    }
+})
+ 
+
+
+
+
+
+
+
+
+    // let name=req.body.name //receive name obj (key and value)send from user
+    // let dob=req.body.dob   //receive dob 
+    // let gender=req.body.gender //gender
+    // let city=req.body.city  //city
+    // let sports=req.body.sports //sports
+    // players.push({name,dob,gender,city,sports}) //using NJSON obj method create key value pair
+//        console.log({ data: players , status: true } )
+//        res.send(  { data: players , status: true }  )//send response to user
+// console.log({add})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
