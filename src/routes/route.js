2aab776501cd89b0f51a5a2a49757fa77cfcb812
shivamp2001 <shivampenshnwar2001@gmail.com>
2022-10-22 21:33:23 +0530

    using mongodb create the books data and fetch all data and also use mongoose

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,7 +6,6 @@ const booksController=require("../controller2/bookcontroller")
 
 
 
-
 router.post('/creatBooks',booksController.createBook)
 router.get('/getbook',booksController.getbook)
 module.exports = router;
